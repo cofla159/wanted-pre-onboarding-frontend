a01a0664c53ae90874bb756b355bf5d222846fac
cofla <cofla159@naver.com>
2022-12-14 21:38:22 +0900

    feat: 로그아웃 버튼

--- client/src/pages/Todo.js
+++ client/src/pages/Todo.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import TodoLists from "../components/TodoLists";
 
 function Todo() {
   return (
     <>
+      <Link
+        to="/"
+        onClick={() => {
+          window.localStorage.removeItem("token");
+        }}
+      >
+        로그아웃
+      </Link>
       <div className="text-2xl">Todo List</div>
       <TodoLists />
     </>
